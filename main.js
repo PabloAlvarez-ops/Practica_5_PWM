8628ece8b2df82a4c6d8da675a926b691b2f6bba
Pablo Álvarez <79628886+PabloAlvarez-ops@users.noreply.github.com>
2022-10-22 19:25:03 +0000

    Falta seguir customizando el juego

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const OPPONENT_HEIGHT = 5,
-    OPPONENT_PICTURE = "assets/malo.png",
+const OPPONENT_HEIGHT = 10, //5
+    OPPONENT_PICTURE = "assets/oponent.png",
     OPPONENT_PICTURE_DEAD = "assets/malo_muerto.png",
     OPPONENT_SPEED = 5, //La velocidad del boss deberá ser el doble a la del oponente
     OPPONENT_WIDTH = 5,
@@ -9,19 +9,19 @@ const OPPONENT_HEIGHT = 5,
     KEY_RIGHT = "RIGHT",
     KEY_SHOOT = "SHOOT",
     MIN_TOUCHMOVE = 20,
-    PLAYER_HEIGHT = 5,
-    PLAYER_PICTURE = "assets/bueno.png",
+    PLAYER_HEIGHT = 10, //5
+    PLAYER_PICTURE = "assets/player.png",
     PLAYER_PICTURE_DEAD = "assets/bueno_muerto.png",
     PLAYER_SPEED = 20,
     PLAYER_WIDTH = 5,
-    SHOT_HEIGHT = 1.5,
+    SHOT_HEIGHT = 6, //3
     SHOT_SPEED = 20,
-    SHOT_PICTURE_PLAYER = "assets/shot1.png",
-    SHOT_PICTURE_OPPONENT = "assets/shot2.png",
-    SHOT_WIDTH = 1.5,
+    SHOT_PICTURE_PLAYER = "assets/peanut_shot1.png",
+    SHOT_PICTURE_OPPONENT = "assets/peanut_shot1.png",
+    SHOT_WIDTH = 3, //1.5
     LIVES = 3;
     BOSS_SPEED = 10,
-    BOSS_PICTURE = "assets/jefe.png",
+    BOSS_PICTURE = "assets/boss.png",
     BOSS_PICTURE_DEAD = "assets/jefe_muerto.png";
 
 
